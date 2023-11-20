58b93e11c04c3fc402f5b4c5430c5426f4dd2fc8
Pavel Yakupov <PVYAKUPOV@rofltech.ru>
2023-11-21 00:32:00 +0400

    fix

--- src/components/MainPart.jsx
+++ src/components/MainPart.jsx
@@ -1,81 +1,118 @@
-import windBlack  from '../assets/windblack.png';
-import dropBlack  from '../assets/drop-black.png';
-import compassBlack from '../assets/compassBlack.png';
-import { useEffect, useState } from 'react';
+import windBlack from "../assets/windblack.png";
+import dropBlack from "../assets/drop-black.png";
+import compassBlack from "../assets/compassBlack.png";
+import { useEffect, useState } from "react";
 
-const MainPart = ({data}) => {
-    const [date, setDate] = useState();
-    const [time, setTime] = useState();
-    const [windDirection, setWindDirection] = useState();
+const MainPart = ({ data }) => {
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+  const [windDirection, setWindDirection] = useState();
 
-    useEffect(()=>{
-        const getOurDate = () => {
-        const myDate = new Date();
-        const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-    
-        let todayDate = myDate.getDate();
-        let month = months[myDate.getMonth()];
-        let year = myDate.getFullYear();
-        let hours = myDate.getHours();
-        let minutes = myDate.getMinutes();
+  useEffect(() => {
+    const getOurDate = () => {
+      const myDate = new Date();
+      const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
 
-        setDate(`${todayDate}` + " " + `${month}` + " " + `${year}`);
-        setTime(`${hours} : ${minutes}`);
-        };
-        getOurDate();
+      let todayDate = myDate.getDate();
+      let month = months[myDate.getMonth()];
+      let year = myDate.getFullYear();
+      let hours = myDate.getHours();
+      let minutes = myDate.getMinutes();
+      //  почему это let а не const?
 
-    },[]);
+      setDate(`${todayDate}` + " " + `${month}` + " " + `${year}`);
+      setTime(`${hours} : ${minutes}`);
+    };
+    getOurDate();
+  }, []);
 
-    useEffect(()=>{
-    const compassSector = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"];
-    if(data) {
-        setWindDirection(compassSector[(data.wind.deg / 22.5).toFixed(0)]);
+  useEffect(() => {
+    const compassSector = [
+      "N",
+      "NNE",
+      "NE",
+      "ENE",
+      "E",
+      "ESE",
+      "SE",
+      "SSE",
+      "S",
+      "SSW",
+      "SW",
+      "WSW",
+      "W",
+      "WNW",
+      "NW",
+      "NNW",
+      "N",
+    ];
+    if (data) {
+      setWindDirection(compassSector[(data.wind.deg / 22.5).toFixed(0)]);
     }
-    },[data])
+  }, [data]);
 
-    return (
-        <div className="mainPart">
-        <h4 className="minMax">
-            <span className="minTemp">{data ? `${Math.round(data.main.temp_min)}°C` : "4°C"}</span>
-            / 
-            <span className="maxTemp">{data ? `${Math.round(data.main.temp_max)}°C` : "7°C"}</span>
-        </h4>
-        <h1 className='temperature'>{data ? `${Math.round(data.main.temp)}°C` : "8°C"}</h1>
-        <p className="conditionals">{data ? `${data.weather[0].main}` : "Clear"}</p>
-        <div className="block1">
-            <div className="box">
-                <p className="box">
-                  <img src={compassBlack} alt="compass"/>
-                  <span className="windDirection"> 
-                   {data ? windDirection : "SSE"}
-                  </span>
-                </p>
-            </div>
-            <div className="box">
-                <p className="box">
-                  <img src={windBlack} alt="windSpeed"/>
-                  <span  className="windSpeed">
-                     {data ? Math.round(data.wind.speed) :"6"}
-                  </span> 
-                  m/s
-                </p>
-            </div>
-            <div className="box">
-                <p  className="box">
-                  <img src={dropBlack} alt="Humidity"/>
-                  <span className="humidity">
-                  {data ? data.main.humidity : "63"}
-                  </span>
-                  %
-                </p>
-            </div>
+  return (
+    <div className="mainPart">
+      <h4 className="minMax">
+        <span className="minTemp">
+          {data ? `${Math.round(data.main.temp_min)}°C` : "4°C"}
+        </span>
+        /
+        <span className="maxTemp">
+          {data ? `${Math.round(data.main.temp_max)}°C` : "7°C"}
+        </span>
+      </h4>
+      <h1 className="temperature">
+        {data ? `${Math.round(data.main.temp)}°C` : "8°C"}
+      </h1>
+      <p className="conditionals">
+        {data ? `${data.weather[0].main}` : "Clear"}
+      </p>
+      <div className="block1">
+        <div className="box">
+          <p className="box">
+            <img src={compassBlack} alt="compass" />
+            <span className="windDirection">
+              {data ? windDirection : "SSE"}
+            </span>
+          </p>
         </div>
-        <div className="time">
-            <p>{date}</p>
-            <h4>{time}</h4>
+        <div className="box">
+          <p className="box">
+            <img src={windBlack} alt="windSpeed" />
+            <span className="windSpeed">
+              {data ? Math.round(data.wind.speed) : "6"}
+            </span>
+            m/s
+          </p>
         </div>
+        <div className="box">
+          <p className="box">
+            <img src={dropBlack} alt="Humidity" />
+            <span className="humidity">{data ? data.main.humidity : "63"}</span>
+            %
+          </p>
+        </div>
+      </div>
+      <div className="time">
+        <p>{date}</p>
+        <h4>{time}</h4>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default MainPart;
--- src/components/OtherInfo.jsx
+++ src/components/OtherInfo.jsx
@@ -1,49 +1,69 @@
-import { useEffect, useState } from 'react';
-import feelsLikeBlack from '../assets/feelsLikeBlack.png';
-import pressureBlack from '../assets/pressureBlack.png';
-import sunriseBlack from '../assets/sunriseBlack.png';
-import sunsetBlack from '../assets/sunsetBlack.png';
-import visibilityBlack from '../assets/visibilityBlack.png';
+import { useEffect, useState } from "react"; // fix delete unneccery
+import feelsLikeBlack from "../assets/feelsLikeBlack.png";
+import pressureBlack from "../assets/pressureBlack.png";
+import sunriseBlack from "../assets/sunriseBlack.png";
+import sunsetBlack from "../assets/sunsetBlack.png";
+import visibilityBlack from "../assets/visibilityBlack.png";
 
-
-
-const OtherInfo = ({data}) => {
-
-    return (
-        <div className="otherInfo">
-            <div className="item">
-                <img src={feelsLikeBlack} alt="FeelsLike"/>
-                <p className="description">Feels like </p>
-                <p className="feelsLike">{data ? `${Math.round(data.main.feels_like)}°C` : "8°C"}</p>
-            </div>
-            <div className="item">
-                <img src={pressureBlack} alt="Pressure"/>
-                <p className="description">Pressure</p>
-                <p className="pressure">{data ? `${data.main.pressure}hPa` : "1010 hPa"}</p>
-            </div>
-            <div className="item">
-                <img src={visibilityBlack} alt="Visibility"/>
-                <p className="description">Visibility</p>
-                <p><span className="visibility">{data ? (Number(`${data.visibility}`)/1000) : "10"}</span> km</p>
-            </div>
-            <div className="item">
-                <img src={sunriseBlack} alt="Sunrise"/>
-                <p className="description">Sunrise</p>
-                <p className="sunrise">{data 
-                ? 
-                `${(new Date((data.sys.sunrise + data.timezone)*1000).getUTCHours()+ ":" +new Date((data.sys.sunrise + data.timezone)*1000).getMinutes())}`
-                : "6:46"}</p>
-            </div>
-            <div className="item">
-                <img src={sunsetBlack} alt="Sunset"/>
-                <p className="description">Sunset</p>
-                <p className="sunset">{data 
-                ? 
-                `${(new Date((data.sys.sunset + data.timezone)*1000).getUTCHours()+ ":" +new Date((data.sys.sunset + data.timezone)*1000).getMinutes())}`
-                : "17:50"}</p>
-            </div>
-        </div>
-    )
-}
+const OtherInfo = ({ data }) => {
+  return (
+    <div className="otherInfo">
+      <div className="item">
+        <img src={feelsLikeBlack} alt="FeelsLike" />
+        <p className="description">Feels like </p>
+        <p className="feelsLike">
+          {data ? `${Math.round(data.main.feels_like)}°C` : "8°C"}
+        </p>
+      </div>
+      <div className="item">
+        <img src={pressureBlack} alt="Pressure" />
+        <p className="description">Pressure</p>
+        <p className="pressure">
+          {data ? `${data.main.pressure}hPa` : "1010 hPa"}
+        </p>
+      </div>
+      <div className="item">
+        <img src={visibilityBlack} alt="Visibility" />
+        <p className="description">Visibility</p>
+        <p>
+          <span className="visibility">
+            {data ? Number(`${data.visibility}`) / 1000 : "10"}
+          </span>{" "}
+          km
+        </p>
+      </div>
+      <div className="item">
+        <img src={sunriseBlack} alt="Sunrise" />
+        <p className="description">Sunrise</p>
+        <p className="sunrise">
+          {data
+            ? `${
+                new Date(
+                  (data.sys.sunrise + data.timezone) * 1000
+                ).getUTCHours() +
+                ":" +
+                new Date((data.sys.sunrise + data.timezone) * 1000).getMinutes()
+              }`
+            : "6:46"}
+        </p>
+      </div>
+      <div className="item">
+        <img src={sunsetBlack} alt="Sunset" />
+        <p className="description">Sunset</p>
+        <p className="sunset">
+          {data
+            ? `${
+                new Date(
+                  (data.sys.sunset + data.timezone) * 1000
+                ).getUTCHours() +
+                ":" +
+                new Date((data.sys.sunset + data.timezone) * 1000).getMinutes()
+              }`
+            : "17:50"}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default OtherInfo;
